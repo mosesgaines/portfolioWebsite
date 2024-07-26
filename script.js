6e66c0649b42a1399377f62235063d6926ff82b0
Moses Gaines <108144354+mosesgaines@users.noreply.github.com>
2024-07-26 19:32:08 -0400

    basically finished

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+    document.addEventListener("mousemove", (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+    
+        const gradientCenterX = (mouseX / window.innerWidth) * 100;
+        const gradientCenterY = (mouseY / window.innerHeight) * 100;
+    
+        const revealedArea = document.querySelector(".spotlight");
+        revealedArea.style.background = `radial-gradient(
+            circle 25rem at ${gradientCenterX}% ${gradientCenterY}%, rgba(51, 76, 184, 0.15), rgba(49, 14, 67, 0)
+        )`;
+    });
+
